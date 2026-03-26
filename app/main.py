b3f8a4fe24324f53bfb8eaca8709b5052c0ebac4
Pranav Rathod <pranav.rathod@iitgn.ac.in>
2026-03-27 00:44:16 +0530

    update CORS with new Vercel URL

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://blablagoa-frontend.vercel.app",
+        "https://blablagoa.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
